4c46775c1edca01c788ee54d7a523f401a5dbae3
Antoine Merino <antoine.merino.dev@gmail.com>
2025-04-04 15:59:01 +0200

    Fix name search with a dash

--- app/utils/sql.py
+++ app/utils/sql.py
@@ -5,7 +5,7 @@ def sanitize_fulltext_string(text: str) -> str:
     """Generate a fulltext SQL string for MATCH AGAINST queries. Remove also special SQL characters (wildcards etc.)"""
     if not text:
         return ""
-    forbidden_patterns = ["(", ")", ":", "\\", "/", "<", ">", "$", "*", "%", "_", "&", '"']
+    forbidden_patterns = ["(", ")", ":", "\\", "/", "<", ">", "$", "*", "%", "_", "&", '"', "-"]
     # print("".join(forbidden_patterns))
     for pattern in forbidden_patterns:
         text = text.replace(pattern, " ")
