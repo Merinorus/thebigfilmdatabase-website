5d1f9b8ac3de7e188ab8683b7f4078e9af10ef4c
Antoine Merino <antoine.merino.dev@gmail.com>
2025-06-03 17:35:18 +0200

    Barcode scanning improvements
    - Keep track of the last camera device used
    - Automatically enable camera if it was recently used
    - When a barcode is found, briefly show it before displaying the results
    - Wait one second between two barcode detections to avoid spamming the API

--- static/scripts/barcode_scanner.js
+++ static/scripts/barcode_scanner.js
@@ -10,7 +10,7 @@ const enableimg = document.getElementById("barcodescanenableimg");
 const resultElement = document.getElementById("result");
 let videostreamstarted = false;
 let currentStream = null;
-
+let scanningEnabled = true;
 
 const ctx = canvas.getContext("2d", { willReadFrequently: true });
 const video = document.createElement("video");
@@ -19,7 +19,6 @@ video.setAttribute("width", canvas.width);
 video.setAttribute("height", canvas.height);
 video.setAttribute("autoplay", "");
 canvas.appendChild(video);
-let nbdetected = 0;
 
 function readBarcodeFromCanvas(canvas, format, tryHarder) {
     var imgWidth = canvas.width;
@@ -39,16 +38,21 @@ function readBarcodeFromCanvas(canvas, format, tryHarder) {
 }
 
 function drawResult(code) {
-    ctx.beginPath();
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = "red";
     with (code.position) {
-        ctx.moveTo(topLeft.x, topLeft.y);
-        ctx.lineTo(topRight.x, topRight.y);
-        ctx.lineTo(bottomRight.x, bottomRight.y);
-        ctx.lineTo(bottomLeft.x, bottomLeft.y);
-        ctx.lineTo(topLeft.x, topLeft.y);
-        ctx.stroke();
+        ctx.font = "bold 28px 'Open Sans', sans-serif";
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 4;
+        // Avoid the text beoing out of the canvas (300 x 225)
+        barcodeX = (topLeft.x + topRight.x + bottomLeft.x + bottomRight.x) / 4;
+        barcodeY = (topLeft.y + topRight.y + bottomLeft.y + bottomRight.y) / 4;
+        x = Math.min(215, Math.max(85, barcodeX));
+        y = Math.min(200, Math.max(25, barcodeY));
+        text = code.text
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.strokeText(text, x, y);
+        ctx.fillStyle = "#fefefe"
+        ctx.fillText(text, x, y)
     }
 }
 
@@ -56,22 +60,31 @@ function escapeTags(htmlStr) {
     return htmlStr.replace(/&/g, "&amp;").replace(/</g , "&lt;").replace( />/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#39;");
 }
 
+function temporaryDisableScanning(){
+    scanningEnabled = false;
+    setTimeout(function () {
+        scanningEnabled = true;
+    }, 1000);
+}
+
 const processFrame = function () {
+    if (scanningEnabled === true){
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
     const code = readBarcodeFromCanvas(canvas, format.value, tryHarder === 'true');
     if (code.format) {
-        nbdetected = (nbdetected + 1) % 10;
-        if (nbdetected === 1) {
+            temporaryDisableScanning()
             resultElement.innerText = code.format + ": " + escapeTags(code.text);
             drawResult(code)
+            setTimeout(function () {
             if (code.format === "ITF") {
                 window.location.href = "search?dx_full=" + encodeURIComponent(code.text);
             } else if (code.format === "DXFilmEdge") {
                 window.location.href = "search?dx_number=" + encodeURIComponent(code.text);
             }
-        }
+            }, 500);
     }
+}
     requestAnimationFrame(processFrame);
 };
 
@@ -103,14 +116,46 @@ const updateVideoStream = function (deviceId) {
         });
 };
 
-enableimg.addEventListener("click", function () {
+function onCameraActivated(){
+    sessionStorage.setItem('lastCameraActivated', Date.now().toString());
+}
+
+function isCameraRecentlyActivated() {
+    const last = sessionStorage.getItem('lastCameraActivated');
+    if (!last) return false;
+
+    const lastTimestamp = parseInt(last, 10);
+    const now = Date.now();
+    const timeout = 5 * 60 * 1000; // 5 minutes
+
+    if (now - lastTimestamp < timeout) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function activateCamera(deviceId = null){
+    onCameraActivated();
     if (!videostreamstarted){
         videostreamstarted = true;
-        updateVideoStream(cameraSelector.value);
+        if (deviceId === null){
+            updateVideoStream(cameraSelector.value);
+        }
+        else{
+            updateVideoStream(deviceId);
+        }
     }
+}
+
+enableimg.addEventListener("click", function () {
+    activateCamera();
 });
 
 cameraSelector.addEventListener("change", function () {
+    onCameraActivated();
+    const selectedDeviceId = this.value;
+    localStorage.setItem("preferredCameraId", selectedDeviceId);
     if (!videostreamstarted){
         videostreamstarted = true;
     }
@@ -121,6 +166,8 @@ if (!navigator.mediaDevices?.enumerateDevices) {
     console.log("enumerateDevices() not supported.");
   } else {
     // List cameras and microphones.
+    const savedDeviceId = localStorage.getItem("preferredCameraId");
+
     navigator.mediaDevices
       .enumerateDevices()
       .then((devices) => {
@@ -136,6 +183,9 @@ if (!navigator.mediaDevices?.enumerateDevices) {
             option.value = device.deviceId;
             option.text = device.label;
             cameraSelector.add(option, null);
+            if (option.value === savedDeviceId){
+                option.selected = true;
+            }
             console.log(device)
           }
         });
@@ -144,3 +194,8 @@ if (!navigator.mediaDevices?.enumerateDevices) {
         console.error(`${err.name}: ${err.message}`);
       });
   }
+
+if (isCameraRecentlyActivated()){
+    const savedDeviceId = localStorage.getItem("preferredCameraId");
+    activateCamera(savedDeviceId);
+}
